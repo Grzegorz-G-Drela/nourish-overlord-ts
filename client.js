ce5f27a6fe6720d06632a4bb229f91a1ca001721
Drelka <47503020+Drelka@users.noreply.github.com>
2026-05-03 21:00:40 +0100

    feat: complete client.js frontend

--- client.js
+++ client.js
@@ -23,5 +23,63 @@ mealForm.addEventListener('submit', async (e) => {
     });
 
     const data = await response.json();
-    console.log(data);
+
+    if (!data.items.length) {
+        mealResults.textContent = 'Meal not found. Try different search.';
+        return;
+    }
+    
+    renderMealResults(data);
 });
+
+function renderMealResults(data) {
+    mealResults.textContent = '';
+
+    data.items.forEach(item => {
+        const card = document.createElement('div');
+
+        const name = document.createElement('h3');
+        name.textContent = item.name;
+
+        const stats = document.createElement('p');
+        stats.textContent = `Calories: ${item.calories} | Protein: ${item.protein_g}g | Fat: ${item.fat_total_g}g | Carbohydrates: ${item.carbohydrates_total_g}g`;
+
+        card.append(name, stats);
+        mealResults.append(card);
+    });
+
+    const reaction = document.createElement('p');
+    reaction.textContent = data.reaction;
+    mealResults.append(reaction);
+}
+
+burnedForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const activity = activityInput.value;
+    const duration = durationInput.value;
+
+    const response = await fetch('http://localhost:3000/api/burned', {
+        method: "POST",
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({ activity, duration }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        burnedResults.textContent = 'Activty not found. Try different search.';
+        return;
+    }
+
+    renderBurnedResults(data);
+});
+
+function renderBurnedResults(data) {
+    burnedResults.textContent = '';
+    
+    const burned = document.createElement('p');
+    burned.textContent = `burned kcal: ${data.burned}`;
+
+    burnedResults.append(burned);
+}
