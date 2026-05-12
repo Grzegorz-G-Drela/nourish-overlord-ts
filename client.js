f8665175172a35d133aa0a5aeac51353ee251e9d
Greg <47503020+Grzegorz-G-Drela@users.noreply.github.com>
2026-05-12 13:44:55 +0100

    feat: add profile section to HTML, add styles

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000' : window.location.origin;
+const BASE_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' ) ? 'http://127.0.0.1:3000' : window.location.origin;
 
 const mealForm = document.querySelector('#meal-form');
 const mealInput = document.querySelector('#meal-input');
